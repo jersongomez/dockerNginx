8f3ec9d2db2fd52514eae73262f0b7b21efe2ed7
Jerson Gomez <jermetrix@gmail.com>
2016-10-03 09:45:18 -0500

    Create conexionPostgres.php
    Se crea archivo de prueba de conexión a postgres

---
+++ conexionPostgres.php
@@ -0,0 +1,10 @@
+<?php
+
+$db_connection = pg_connect("host=postgres dbname=postgres user=postgres password=123456");
+if(!$db_connection){
+      echo "Error: Conectando postgres <br/>";
+      print_r(pg_last_error());
+   } else {
+      echo "Conexion abierta<br/>";
+      var_dump($db_connection);
+   }
